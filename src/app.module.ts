1bfcd1007918bb360c67f946a2b9797d889e7dc5
RanaJazim <stackdeveloper87@gmail.com>
2021-06-02 23:59:36 -0700

    added nestjs config module and connect database with these configs ..

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Inject, Injectable, Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MongooseModule } from "@nestjs/mongoose";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import configuration from "./config/configuration";
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ load: [configuration] }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get("MONGO_URI"),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
