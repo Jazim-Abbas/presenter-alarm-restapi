2261f3f1ba31552bdcbeb80219a2747592b0da40
RanaJazim <stackdeveloper87@gmail.com>
2021-06-03 01:57:42 -0700

    validate the incoming request when create superuser ..

--- src/auth/dtos/create-user.dto.ts
+++ src/auth/dtos/create-user.dto.ts
@@ -1,7 +1,17 @@
+import { IsEmail, IsString, Max, MaxLength, Min, MinLength } from "class-validator";
+
 export class CreateUserDto {
+  @IsString()
+  @MinLength(5)
+  @MaxLength(255)
   name: string;
 
+  @IsString()
+  @IsEmail()
   email: string;
 
+  @IsString()
+  @MinLength(8)
+  @MaxLength(40)
   password: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
-  const PORT  = process.env.PORT || 8000;
+  const PORT = process.env.PORT || 8000;
   await app.listen(PORT);
 }
 bootstrap();
