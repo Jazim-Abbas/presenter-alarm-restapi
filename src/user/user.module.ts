c8a9c27489743b731e48d8645316679a5ce07676
RanaJazim <stackdeveloper87@gmail.com>
2021-06-03 01:17:49 -0700

    hash the password before saving to database ..

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,39 @@
 import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
+import * as bcrypt from "bcryptjs";
 import { UserEntity, UserSchema } from "./entities/user.entity";
 import { UserService } from "./user.service";
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: UserEntity.name, schema: UserSchema }]),
+    MongooseModule.forFeatureAsync([
+      {
+        name: UserEntity.name,
+        useFactory: () => {
+          const schema = UserSchema;
+
+          schema.pre("save", function (next) {
+            const user = this;
+
+            if (!user.password) return next();
+
+            if (!user.isModified("password")) return next();
+
+            bcrypt.genSalt(10, function (err, salt) {
+              if (err) return next(err);
+
+              bcrypt.hash(user.password, salt, function (err, hash) {
+                if (err) return next(err);
+                user.password = hash;
+                next();
+              });
+            });
+          });
+
+          return schema;
+        },
+      },
+    ]),
   ],
   providers: [UserService],
   exports: [UserService],
