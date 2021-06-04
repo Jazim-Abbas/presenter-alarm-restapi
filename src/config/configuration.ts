57ae9ebb1d6f97d23a78ca87914a99f5a39c2e7a
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-04 07:27:53 +0500

    spin up the mongodb server using docker and connect to our app ..

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,3 +1,8 @@
-export default () => ({
-  MONGO_URI: "mongodb://localhost:27017/presenter-alarm-app",
-});
+export default () => {
+  const username = process.env.MONGO_USERNAME || "root";
+  const password = process.env.MONGO_PASSWORD || "password";
+
+  return {
+    MONGO_URI: `mongodb://${username}:${password}@localhost:27017/presenter-alarm-app?authSource=admin`,
+  };
+};
