69f9952c6ac68e3ff54c7af61028a9736abf89db
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-16 15:29:54 +0500

    stop timer if any exists ..

--- src/timer/timer.gateway.ts
+++ src/timer/timer.gateway.ts
@@ -9,17 +9,28 @@ import { TimerService } from "./timer.service";
 @WebSocketGateway()
 export class TimerGateway {
   @WebSocketServer()
-  server: Server;
+  private server: Server;
+  private timerFunc: NodeJS.Timeout;
 
   constructor(private readonly timerService: TimerService) {}
 
   @SubscribeMessage("start-timer")
   startTimer() {
-    setInterval(() => {
-      this.timerService.incrementTimer();
-      const time = this.timerService.getTimeAsClock();
+    if (!this.timerFunc) {
+      this.timerFunc = setInterval(() => {
+        this.timerService.incrementTimer();
+        const time = this.timerService.getTimeAsClock();
 
-      this.server.emit("get-timer", time);
-    }, 1000);
+        this.server.emit("get-timer", time);
+      }, 1000);
+    }
+  }
+
+  @SubscribeMessage("stop-timer")
+  stopTimer() {
+    if (this.timerFunc) {
+      clearInterval(this.timerFunc);
+      this.timerService.resetTimer();
+    }
   }
 }
