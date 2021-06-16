fa3c84e110eb84ae6e2209b9b0adc53c04881486
Jazim-Abbas <jazimabbas87@gmail.com>
2021-06-16 11:10:53 +0500

    delete question from live question ..

--- src/live-question/live-question.gateway.ts
+++ src/live-question/live-question.gateway.ts
@@ -1,12 +1,29 @@
-import { SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
+import {
+  MessageBody,
+  SubscribeMessage,
+  WebSocketGateway,
+} from "@nestjs/websockets";
+import { WSExceptionInterceptor } from "src/common/decorators/ws-exception.decorator";
+import { WsValidationPipe } from "src/common/decorators/ws-validation.decorator";
+import { DeleteQuestionIdDto } from "src/common/dtos/delete-question-id.dto";
 import { LiveQuestionService } from "./live-question.service";
 
+@WSExceptionInterceptor()
 @WebSocketGateway()
 export class LiveQuestionGateway {
   constructor(private readonly liveQuestionService: LiveQuestionService) {}
 
   @SubscribeMessage("message")
-  handleMessage(client: any, payload: any): string {
+  handleMessage() {
     return "Hello world!";
+  }
+
+  @WsValidationPipe()
+  @SubscribeMessage("delete-live-question")
+  async saveLiveQuestion(
+    @MessageBody() deleteQuestionDto: DeleteQuestionIdDto
+  ) {
+    await this.liveQuestionService.deleteQuestion(deleteQuestionDto);
+    return { message: "Successfully delete question from live questions " };
   }
 }
--- src/live-question/live-question.service.ts
+++ src/live-question/live-question.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
+import { MoveQuestionDto } from "src/common/dtos/move-question.dto";
 import { GeneralQuestionService } from "src/common/services/general-question.service";
 import { QuestionService } from "src/question/question.service";
 import { LiveQuestionEntity } from "./entity/live-question.entity";
@@ -13,5 +14,9 @@ export class LiveQuestionService extends GeneralQuestionService<LiveQuestionEnti
     protected readonly questionService: QuestionService
   ) {
     super(liveQuestionModel, questionService);
+  }
+
+  async deleteQuestion(moveQuestionDto: MoveQuestionDto) {
+    return this.deleteQuestionFromSection(moveQuestionDto);
   }
 }
