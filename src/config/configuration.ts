4592446fdbbfecd658aaec43706b6ab8c0fe849c
Jazim-Abbas <jazimabbas87@gmail.com>
2021-07-16 14:59:30 +0500

    added production configuration for mongo ..

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,9 +1,10 @@
 export default () => {
   const username = process.env.MONGO_USERNAME || "root";
   const password = process.env.MONGO_PASSWORD || "password";
+  const mongo_URI = `mongodb://${username}:${password}@localhost/presenter-alarm-app?authSource=admin`;
 
   return {
-    MONGO_URI: `mongodb://${username}:${password}@localhost/presenter-alarm-app?authSource=admin`,
-    SECRETKEY: 'mykey'
+    MONGO_URI: process.env.MONGO_URI || mongo_URI,
+    SECRETKEY: "mykey",
   };
 };
